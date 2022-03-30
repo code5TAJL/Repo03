c149cd63951a2018c8049f333d65707827d9f3ce
code5TAJL <asmir.sulejmanovic@outlook.com>
2022-03-30 13:46:06 +0200

    Added second JS file

---
+++ ex02/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+    // Only change code below this line
+    // Atleast five letters in the house
+    /*
+    More than five letters in the house
+    */
+    // Only change code above this line
+
+}
+
+console.log(main());
+
+module.exports = main;
