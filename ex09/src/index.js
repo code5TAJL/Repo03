2efb8ba330dd6366702f7765b8e8886220973ad7
code5TAJL <asmir.sulejmanovic@outlook.com>
2022-03-30 17:20:22 +0200

    Added JS file #9

---
+++ ex09/src/index.js
@@ -0,0 +1,17 @@
+function main(numOne, numTwo) {
+    // Only changed code below this line
+    var difference;
+    var numOne;
+    var numTwo;
+
+    difference = numOne - numTwo;
+    // Only changed code above this line
+    return difference;
+
+}
+
+console.log(main(15, 8)); // change this line
+console.log(main(25, 8)); // change this line
+console.log(main(425, 120)); // change this line
+
+module.exports = main;
